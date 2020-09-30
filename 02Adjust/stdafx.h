bb06380aea701070d9c7b3ec32b6a4853a390e27
acebearxp <acebearxp@msn.com>
2020-09-30 14:41:46 +0800

    remove x86 config

--- 02Adjust/stdafx.h
+++ 02Adjust/stdafx.h
@@ -2,3 +2,11 @@
 
 #include <Windows.h>
 #include <windowsx.h>
+
+#include <opencv2/opencv.hpp>
+
+#ifdef _DEBUG
+	#pragma comment(lib, "opencv_world440d.lib")
+#else
+	#pragma comment(lib, "opencv_world440.lib")
+#endif //  DEBUG
